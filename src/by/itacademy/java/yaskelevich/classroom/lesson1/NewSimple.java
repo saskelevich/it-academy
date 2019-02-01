cf3488027a0fded8492d8294aef9743910208647
114-16\St <yyy@mail.ru>
2019-02-01 21:38:54 +0300

    no message

---
+++ src/by/itacademy/java/yaskelevich/classroom/lesson1/NewSimple.java
@@ -0,0 +1,12 @@
+package by.itacademy.java.yaskelevich.classroom.lesson1;
+
+public class NewSimple {
+	public static void main(String[] args) {
+		double d1 = 0.3d - 0.2d;
+		double d2 = 0.2d - 0.1d;
+		System.out.println("d1 = " + d1 );
+		System.out.println("d2 = " + d2 );
+		System.out.println("d2 == d1 =>" + (d2 == d1));
+		
+	}
+}
