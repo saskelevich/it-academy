01d065b4b424a75af7951aed72c3e84de840674a
114-16\St <yyy@mail.ru>
2019-02-01 21:27:54 +0300

    no message

--- src/by/itacademy/java/yaskelevich/classroom/lesson1/Sample.java
+++ src/by/itacademy/java/yaskelevich/classroom/lesson1/Sample.java
@@ -1,5 +1,18 @@
 package by.itacademy.java.yaskelevich.classroom.lesson1;
 
 public class Sample {
+	public static void main(String[] args) {
 
+		int a = 1;
+		System.out.println("a = " + a);
+
+		final int b = a;
+		System.out.println("b = " + b);
+
+		System.out.println("change a...");
+		a = 2;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
 }
