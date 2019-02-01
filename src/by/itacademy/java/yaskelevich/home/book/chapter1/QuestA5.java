ae77355d1715de00f99ddd33970289b2f05018b9
Stas_Yaskelevich <saskelevic@gmail.com>
2019-02-02 02:05:47 +0300

    hw end chapter1.QuestA

---
+++ src/by/itacademy/java/yaskelevich/home/book/chapter1/QuestA5.java
@@ -0,0 +1,18 @@
+package by.itacademy.java.yaskelevich.home.book.chapter1;
+
+public class QuestA5 {
+	public static void main(String[] args) {
+
+		int summary = 0;
+		
+		if (args.length > 0) {
+			int[] array = new int[args.length];
+			for (int i = 0; i < args.length; i++) {
+				array[i] = Integer.parseInt(args[i]);
+				summary += array[i];
+			}
+			System.out.println("Сумма = " + summary);
+		}else
+			System.out.println("Don't write data");
+	}
+}
