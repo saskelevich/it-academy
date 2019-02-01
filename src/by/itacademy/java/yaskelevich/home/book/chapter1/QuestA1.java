9d3ab36b8822ccf121bb0991d3f9300ff323605d
Stas_Yaskelevich <saskelevic@gmail.com>
2019-02-02 01:02:06 +0300

    hw

--- src/by/itacademy/java/yaskelevich/home/book/chapter1/QuestA1.java
+++ src/by/itacademy/java/yaskelevich/home/book/chapter1/QuestA1.java
@@ -1,7 +1,10 @@
 package by.itacademy.java.yaskelevich.home.book.chapter1;
 
-public class QuestA {
+public class QuestA1 {
 	public static void main(String[] args) {
-		
+
+		if (args.length != 0)
+			System.out.println("Hello " + args[0]);
 	}
+
 }
