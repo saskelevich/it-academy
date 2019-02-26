6e246d916306937644a60644ef9ac9dbf530d361
114-16\St <yyy@mail.ru>
2019-02-26 21:37:07 +0300

    no message

--- src/by/itacademy/java/yaskelevich/classroom/lessons8/A.java
+++ src/by/itacademy/java/yaskelevich/classroom/lessons8/A.java
@@ -1,8 +1,10 @@
 package by.itacademy.java.yaskelevich.classroom.lessons8;
 
+import java.util.Date;
+
 public class A {
 
     public void test() {
-
+        final Date date = new Date();
     }
 }
---
+++ src/by/itacademy/java/yaskelevich/classroom/lessons9/InnerClass0.java
@@ -0,0 +1,5 @@
+package by.itacademy.java.yaskelevich.classroom.lessons9;
+
+public class InnerClass0 {
+
+}
